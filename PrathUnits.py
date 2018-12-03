675b80916002fb5a03c95900c9fdf311f86e5a8c
Pratham Desai <prathamdesai13@gmail.com>
2018-12-03 11:34:08 -0500

    added data arg in universal terms func

--- PrathUnits.py
+++ PrathUnits.py
@@ -1,8 +1,4 @@
-# probability map
-# generates a superimposed map of all the colour maps elicited from speakers of a language,
-# for evaluation of prediction models
-
-LANGUAGE = 1
+import numpy as np
 
 # generates majority colour map for a specified language
 # cmap should be randomized on ties
@@ -20,15 +16,31 @@ def consolidate_map(pmap):
     return cmap
 
 # prototype model stuff
-def universal_terms(language):
+def universal_terms(data, language):
     """
     Generates a list of colour terms used by any of the speakers of a given language
     """
     universal_terms = set()
-    for speaker in fociDictionary[language]:
-        for term in fociDictionary[language][speaker]:
+    for speaker in fociDictionary[data][language]:
+        for term in fociDictionary[data][language][speaker]:
             universal_terms.add(term)
     return list(universal_terms)
+
+# calculates the euclidean distance between two vectors
+def euclidean_dist(x, y):
+
+    return np.linalg.norm(x, y)
+    
+
+def similarity_func(x, y):
+
+    return np.exp(-euclidean_dist(x, y))
+
+# all cells exemplar prediction function
+# uses a similarity function to categorize new data points
+def all_cells_exemplar_predict(cmap):
+
+
 
 
 def prototype_predict(cmap):
