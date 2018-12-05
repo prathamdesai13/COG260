d9dafa40d4765e77608696eab1594c090255166b
Pratham Desai <prathamdesai13@gmail.com>
2018-12-05 17:48:05 -0500

    fixed some code

--- PrathUnits.py
+++ PrathUnits.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sp
+import scipy.spatial as sp
 from random import shuffle
 from wcs_helper_functions import *
 
@@ -22,7 +22,7 @@ def consolidate_map(pmap):
             if pmap[cell][colour] == max_votes:
                 max_colours.append(colour)
         shuffle(max_colours) # shuffle colours to allow randomness in ties
-        cmap[cell] = {max_colours[0] : max_votes}
+        cmap[cell] = max_colours[0]
     return cmap
 
 # prototype model stuff
@@ -39,7 +39,7 @@ def universal_terms(data, language):
 # calculates the euclidean distance between two vectors
 def euclidean_dist(x, y):
 
-    return sp.spatial.distance.euclidean(x, y)
+    return sp.distance.euclidean(x, y)
     
 
 def similarity_func(x, y):
@@ -48,8 +48,31 @@ def similarity_func(x, y):
 
 # all cells exemplar prediction function
 # uses a similarity function to categorize new data points
-def all_cells_exemplar_predict(cmap):
-    pass
+def all_cells_exemplar_predict(cmap, language):
+    num_cells = 330
+    out_map = [None for _ in range(num_cells)]
+    sims = {}
+    clabDictionary = readClabData('./WCS_data_core/cnum-vhcm-lab-new.txt')
+    for cell in cmap.keys():
+        colour_term = cmap[cell]
+        if colour_term not in sims:
+            sims[colour_term] = 0
+    print(sims)
+    for cell in range(num_cells):
+        (n1, n2, n3) = clabDictionary[cell + 1]
+        clab_coord = (float(n1), float(n2), float(n3))
+        for term in sims:
+            for cell_i in cmap.keys():
+                if term == cmap[cell_i]:
+                    (n1, n2, n3) = clabDictionary[cell_i]
+                    cell_i_coord = (float(n1), float(n2), float(n3))
+                    sims[term] += similarity_func(clab_coord, cell_i_coord)
+        max_key = max(sims, sims.get)
+        out_map[cell] = max_key
+    print(sims)
+    return out_map
+
+
 
 
 def assign_colour(cell_coord, prototypes, func):
@@ -69,7 +92,7 @@ def corresponding_munsell(cell):
     """
     munsell_info = load_munsell()
     index_to_coord = munsell_info[1][cell + 1]
-    return (index_to_coord[0]).join(str(index_to_coord[1]))
+    return str(index_to_coord[0] + str(index_to_coord[1]))
 
 
 def prototype_predict(prototypes):
@@ -80,18 +103,11 @@ def prototype_predict(prototypes):
     """
     num_cells = 330
     out_map = [None for _ in range(num_cells)]
+    clabDictionary = readClabData('./WCS_data_core/cnum-vhcm-lab-new.txt')
     for cell in range(num_cells):
-        # write function that takes the cell, corresponding to munsell color chart, adn then convert to cielab coord
-        # cell_coord = munsell2ceilab(corresponding_munsell(cell)) # need to implement corresponding_munsell
-        cell_coord = None
+        cell_coord = clabDictionary[cell]
         out_map[cell] = assign_colour(cell_coord, prototypes, euclidean_dist)
     return out_map
-
-def cielab2cell(cielab_coord):
-    """
-    converts cielab coordinate to a cell in munsell color chips [0, 330)
-    """
-    pass
 
 def foci_predict(exemplars):
     """
